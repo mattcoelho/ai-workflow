8729602cfb0cc992bd68960b95bea634a29c5dde
mattcoelho <mattco>
2026-02-24 10:54:26 -0800

    adding reasoning, relevance scoring, job summarization

--- job-monitor/companies.py
+++ job-monitor/companies.py
@@ -7,6 +7,7 @@ COMPANIES = [
     {"name": "Honeycomb", "type": "greenhouse", "board_token": "honeycomb"},
     {"name": "Calendly", "type": "greenhouse", "board_token": "calendly"},
     {"name": "GitLab", "type": "greenhouse", "board_token": "gitlab"},
+    {"name": "Human Interest", "type": "greenhouse", "board_token": "humaninterest"},
     # Ashby companies
     {"name": "1Password", "type": "playwright", "board_token": "https://jobs.ashbyhq.com/1password"},
     {"name": "Kraken", "type": "playwright", "board_token": "https://jobs.ashbyhq.com/kraken.com"},
@@ -23,6 +24,15 @@ COMPANIES = [
     {"name": "Twilio", "type": "playwright", "board_token": "https://www.twilio.com/en-us/company/jobs"},
     {"name": "Sourcegraph", "type": "playwright", "board_token": "https://sourcegraph.com/jobs"},
     {"name": "Dropbox", "type": "playwright", "board_token": "https://jobs.dropbox.com/all-jobs"},
+    {"name": "Airbnb", "type": "playwright", "board_token": "https://careers.airbnb.com/positions/"},
+    {"name": "Akamai Technologies", "type": "playwright", "board_token": "https://fa-extu-saasfaprod1.fa.ocs.oraclecloud.com/hcmUI/CandidateExperience/en/sites/CX_1/jobs"},
+    {"name": "Alliants", "type": "playwright", "board_token": "https://alliants.careers.hibob.com/jobs"},
+    {"name": "Bonterra", "type": "playwright", "board_token": "https://bonterra.wd1.myworkdayjobs.com/bonterratech"},
+    {"name": "Dayforce", "type": "playwright", "board_token": "https://jobs.dayforcehcm.com/en-US/mydayforce/alljobs"},
+    {"name": "HubSpot", "type": "playwright", "board_token": "https://www.hubspot.com/careers/jobs?page=1"},
+    {"name": "Sinch", "type": "playwright", "board_token": "https://iaings.fa.ocs.oraclecloud.com/hcmUI/CandidateExperience/en/sites/CX_1/jobs"},
+    {"name": "Synoptek", "type": "playwright", "board_token": "https://careers.synoptek.com/jobs"},
+    {"name": "Toast", "type": "playwright", "board_token": "https://careers.toasttab.com/jobs/search"},
     # Static HTML companies
     {"name": "Aha!", "type": "static", "board_token": "https://www.aha.io/company/careers/current-openings"},
     {"name": "DuckDuckGo", "type": "static", "board_token": "https://duckduckgo.com/hiring"},
